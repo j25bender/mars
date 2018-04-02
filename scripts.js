51c9e59d972e43b98fa1c6b2bfdb3bc1814d001b
Jeff Bender <j25bender@gmail.com>
2018-04-02 13:24:16 -0600

    server set up

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ $('#add-item-button').click(() => {
   const itemToAdd = $('#item-to-pack').val()
   $('#item-container').append(`<div class="item-card">
                                 <h1>${itemToAdd}</h1>
-                                <button class=${itemToAdd} onclick=hello() >Delete</button>
+                                <button class=${itemToAdd} onclick=deleteMe() >Delete</button>
                                 <input class="checkBox" type="checkbox">
                                </div>`)
 })
 
-const hello = () => {
+const deleteMe = () => {
   $(this).click((e) => {
     const deleteThis = '.' + e.target.className
     $(deleteThis).parent().remove()
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json())
+
+app.listen(app.get('port'), () => {
+  console.log(`App running on ${app.get('port')}`)
+})
